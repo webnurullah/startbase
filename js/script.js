c81309bd92f19baf10ee1889e7e0c2cd8c350578
Farabi islam Nurullah <farabi48islam@gmail.com>
2022-06-21 00:58:06 +0600

    home done

---
+++ js/script.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+    $('.brandlogo').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 5000,
+                settings: {
+                
+                  slidesToShow: 4
+                }
+              },
+            {
+                breakpoint: 992,
+                settings: {
+                
+                  slidesToShow: 3
+                }
+              },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 576,
+              settings: {
+                slidesToShow: 2
+              }
+            }
+          ]
+   
+    });
+
+    // news slider
+    $('.newslider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 5000,
+                settings: {
+                
+                  slidesToShow: 3
+                }
+              },
+            {
+                breakpoint: 992,
+                settings: {
+                
+                  slidesToShow: 3
+                }
+              },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 576,
+              settings: {
+                slidesToShow: 1
+              }
+            }
+          ]
+   
+    });
+  });
