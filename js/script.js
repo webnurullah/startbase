54835588544becb48db517bcd6816329d6721898
Farabi islam Nurullah <farabi48islam@gmail.com>
2022-06-25 03:13:16 +0600

    final update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
 
 
@@ -94,53 +95,38 @@ $(document).ready(function(){
           ]
    
     });
-    // price slider
-    $('.priceslider').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        arrows: false,
-        dots: false,
-        responsive: [
-            {
-                breakpoint: 5000,
-                settings: {
-                
-                  slidesToShow: 3
-                }
-              },
-            {
-                breakpoint: 992,
-                settings: {
-                
-                  slidesToShow: 2
-                }
-              },
-            {
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 1
-              }
-            },
-            {
-              breakpoint: 576,
-              settings: {
-                slidesToShow: 1
-              }
-            }
-          ]
+    
    
+  
+
+
+});
+
+
+// price slider
+  $(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+      loop:false,
+      margin:15,
+      nav:false,
+      dots:false,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:1
+              
+          },
+          768:{
+              items:2
+              
+          },
+          992:{
+              items:3
+              
+          }
+      }
     });
-
-
-
   });
 
 
-
-
-
-
-
+ 
